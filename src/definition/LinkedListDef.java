90c51f89691a7db28532f68144535b4307820e37
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-02-23 17:59:54 +0530

    made node class of generic type

--- src/definition/LinkedListDef.java
+++ src/definition/LinkedListDef.java
@@ -5,6 +5,8 @@ import adt.LinkedListADTInterface;
 import java.util.Iterator;
 
 public class LinkedListDef<E> implements LinkedListADTInterface<E>, Iterable<E> {
+    public int size = 0;
+    private Node<Person> head = null;
 
 
     @Override
@@ -25,5 +27,28 @@ public class LinkedListDef<E> implements LinkedListADTInterface<E>, Iterable<E> 
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        private Node(E data) {
+            this.data = data;
+        }
+
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        private E getData() {
+
+            return data;
+        }
+
+        private Node<E> getNext() {
+            return next;
+        }
     }
 }
