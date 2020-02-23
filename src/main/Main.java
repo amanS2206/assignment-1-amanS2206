666a2a532e735f9bea44b306d4fa62c6fb208dc6
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-02-23 22:10:30 +0530

    made Scanner class object

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,14 @@ package main;
 
 import definition.Helper;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Helper h = new Helper();
         boolean choice = true;
         while (choice) {
+            Scanner sc = new Scanner(System.in);
 
         }
     }
