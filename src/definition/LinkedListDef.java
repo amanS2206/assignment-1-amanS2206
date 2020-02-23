8c83f909c39c7031885d1f7865ddd8215e57b871
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-02-23 17:57:00 +0530

    made linked list definition class of generic types which implements linked list adt and iterable adt

---
+++ src/definition/LinkedListDef.java
@@ -0,0 +1,29 @@
+package definition;
+
+import adt.LinkedListADTInterface;
+
+import java.util.Iterator;
+
+public class LinkedListDef<E> implements LinkedListADTInterface<E>, Iterable<E> {
+
+
+    @Override
+    public void add(Person item) {
+
+    }
+
+    @Override
+    public int search(Person item) {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
