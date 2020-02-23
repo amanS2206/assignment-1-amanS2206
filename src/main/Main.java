2adc90bf165dc42ced544a76d4d979b254952fd6
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-02-23 22:08:01 +0530

    made object Of Helper class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import definition.Helper;
+
 public class Main {
     public static void main(String[] args) {
-
+        Helper h = new Helper();
+        boolean choice = true;
     }
 }
