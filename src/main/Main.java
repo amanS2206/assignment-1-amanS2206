df8daf00a4c5c8b0adcc7db8cb1fe524ac4f49a5
AMANsaxena2206 <40917931+AMANsaxena2206@users.noreply.github.com>
2020-02-23 22:11:25 +0530

    made menu for contact list

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,12 @@ public class Main {
         boolean choice = true;
         while (choice) {
             Scanner sc = new Scanner(System.in);
+            System.out.println("Welcome to the contact list");
+            System.out.println("Press 1 to add a new contact");
+            System.out.println("Press 2 to view all contacts");
+            System.out.println("Press 3 to search for a contact");
+            System.out.println("Press 4 to delete a contact");
+            System.out.println("Press 5 to exit program ");
 
         }
     }
